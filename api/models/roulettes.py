b79d32511bc5e3f60d79573c4b1b6b2b8061387c
Sergio Parada <sdparada97@gmail.com>
2023-06-29 01:14:36 -0500

    🆕 feature: Add Patch endpoint : Open Roulette

--- api/models/roulettes.py
+++ api/models/roulettes.py
@@ -13,11 +13,10 @@ class Roulette(db.Model):
     total_bet = db.Column(db.Numeric(),default=0, nullable=False)
     is_active = db.Column(db.Enum(RouletteStatus,
                                     values_callable=lambda x: [str(e.value) for e in RouletteStatus]),
-                            default=RouletteStatus.OPEN)
+                            default=None)
     
     def __init__(self):
         self.total_bet = 0
-        self.is_active = RouletteStatus.OPEN.value
     
     def __repr__(self):
         return f"<Roulette {self.id}>"
--- api/roulette/views.py
+++ api/roulette/views.py
@@ -1,7 +1,7 @@
 from flask_restx import Namespace, Resource, fields
 from flask_jwt_extended import jwt_required
 
-from ..models.roulettes import Roulette
+from ..models.roulettes import Roulette, RouletteStatus
 
 from http import HTTPStatus
 
@@ -12,6 +12,14 @@ roulette_model = roulette_namespace.model(
             'id':fields.Integer()
     }
 )
+
+patch_model = roulette_namespace.model(
+        'Roulette_Patch',{
+            'id':fields.Integer(),
+            'message': fields.String()
+    }
+)
+
 
 @roulette_namespace.route('/Roulette')
 class RouletteCreate(Resource):
@@ -27,8 +35,18 @@ class RouletteCreate(Resource):
 
 @roulette_namespace.route('/Roulette/open/<int:roulette_id>')
 class RouletteOpen(Resource):
+    @roulette_namespace.marshal_with(patch_model)
+    @jwt_required()
     def patch(self,roulette_id):
-        pass
+        roulette = Roulette.query.get(roulette_id)
+        
+        if not roulette:
+            roulette_namespace.abort(404, f'roulette {roulette_id} not found')
+        
+        roulette.is_active = RouletteStatus.OPEN
+        roulette.save()
+        
+        return {'id': roulette.id,'message': 'ROULETTE IS OPEN '}, HTTPStatus.OK
 
 @roulette_namespace.route('/Roulette/close/<int:roulette_id>')
 class RouletteClose(Resource):
